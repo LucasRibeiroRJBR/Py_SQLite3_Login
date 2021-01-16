3eedfafa55630b10a4c4ed375ba6acfd3fc95727
LucasRibeiroRJBR <lucasribeirorjbr@gmail.com>
2021-01-16 15:00:09 -0300

    Data implements

--- login_cmd.py
+++ login_cmd.py
@@ -7,31 +7,32 @@ from ttkthemes import ThemedStyle
 pyglet.font.add_file("font/HYWenHei.ttf")
 
 def login():
-    with sqlite3.connect('login.db') as db:
-        c = db.cursor()
 
-    find_user = ("SELECT * FROM users WHERE user = ? AND password = ?")
+    conn = sqlite3.connect('db/login.db')
+    c = conn.cursor()
 
-    c.execute(find_user, [(input_user, input_password)])
+    c.execute(f'SELECT * FROM users WHERE user = "{VarUser.get()}" AND password = "{VarPassword.get()}"')
 
     resul = c.fetchall()
     
     if resul:
         for i in resul:
-            resultado['text'] = f"Bem-vindo, {i[0]}!"
+            resultado['text'] = f"Bem-vindo(a), {i[0]}!"
     else:
-        resultado['text'] = f"Usuário ou senha inválidos!"
+        resultado['text'] = f"Dados inválidos!"
 
     
 
 root = Tk()
 root.title('SQ System')
-root.geometry('300x350')
+#root.geometry('200x350')
 root.configure(background = '#202020')
 
 style = ThemedStyle()
 style.set_theme('clam')
-style.configure("my.TLabel", font=('HYWenHei-85W', 24, 'bold'), bg = '#202020', borderwidth = 0)
+style.configure("my.TLabel", font=('HYWenHei-85W', 24, 'bold'), background = '#202020', foreground = 'white', borderwidth = 0)
+style.configure("my.TButton", font=('HYWenHei-85W', 24, 'bold'), borderwidth = 0)
+style.configure("my.TEntry", font=('HYWenHei-85W', 14, 'bold'), padding = 5)
 
 # IMAGENS
 img_login = PhotoImage(file="img/login.png")
@@ -50,14 +51,17 @@ lb_password.grid(row = 2, column = 0)
 VarUser = StringVar()
 VarPassword = StringVar()
 
-input_user = ttk.Entry(root)
-input_user.grid(row = 1, column = 1)
+input_user = ttk.Entry(root, font=('HYWenHei-85W', 14, 'bold'), width = 25, textvariable = VarUser)
+input_user.grid(row = 1, column = 1, padx = 15)
 
-input_password = ttk.Entry(root, show = '*')
-input_password.grid(row = 2, column = 1)
+input_password = ttk.Entry(root, show = '*', font=('HYWenHei-85W', 14, 'bold'), width = 25, textvariable = VarPassword)
+input_password.grid(row = 2, column = 1, padx = 15)
+
+bt_consultar = ttk.Button(root, text = 'Consultar', command = login, style = "my.TButton")
+bt_consultar.grid(row = 3, columnspan = 2)
 
 resultado = ttk.Label(root, text = '', style = "my.TLabel")
-resultado.grid(row = 3, columnspan = 2)
+resultado.grid(row = 4, columnspan = 2, pady = 30)
 
 
 
